d7de965d58ef11ebeaf5c8cd6946e6c6d5227470
Oghenetega Esedere <arcteggzz@gmail.com>
2023-07-29 00:02:47 +0100

    feat: added google maps using api key

--- app/(publicPages)/home/components/MapContainer.tsx
+++ app/(publicPages)/home/components/MapContainer.tsx
@@ -1,12 +1,26 @@
 "use client";
 
 import styles from "../styles/MapContainer.module.scss";
+import { useMemo } from "react";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 
 const MapContainer = () => {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string,
+  });
+
+  const center = useMemo(() => ({ lat: 6.5, lng: 3.3 }), []);
+
+  if (!isLoaded) return <div className={styles.MapContainer}>Loading...</div>;
+
   return (
-    <div className={styles.MapContainer}>
-      <p>MapContainer</p>
-    </div>
+    <GoogleMap
+      zoom={10}
+      center={center}
+      mapContainerClassName={styles.MapContainer}
+    >
+      <Marker position={center} />
+    </GoogleMap>
   );
 };
 
